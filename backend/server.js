9abfad014be1ea70d4e16630107260bca693e2ad
balazstrencsenyi <b.trencsenyi@gmail.com>
2023-06-12 15:58:06 +0200

    express set up

---
+++ backend/server.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const app = express();
+const port = 2000;
+const path = require('path');
+const fs = require('fs');
+const fileUpload = require('express-fileupload');
+
+const FE_FS_PATH = path.join(__dirname, "..", "frontend");
+
+app.use(express.static(FE_FS_PATH));
+app.use(express.json());
+
