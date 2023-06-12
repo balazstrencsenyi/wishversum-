9f85cb63e48e3006320ed6e0eaecf6353de95877
balazstrencsenyi <b.trencsenyi@gmail.com>
2023-06-12 17:12:48 +0200

    added wish page

--- frontend/script.js
+++ frontend/script.js
@@ -1,10 +1,10 @@
-import { createHeader } from './model/createDom.js';
+import { createHeader, createLandingPage, createWishForm } from './model/createDom.js';
 
 const root = document.getElementById("root")
 
 function main () {
   
-  root.append(createHeader())
+  root.append(createHeader(), createLandingPage(), createWishForm())
 }
 
 window.onload = () => {
