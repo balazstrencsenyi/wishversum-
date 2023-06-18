a247ecde650c04a8da6ab4e6545a182ffc3c9de0
balazstrencsenyi <b.trencsenyi@gmail.com>
2023-06-18 21:22:10 +0200

    added sumup to cart

--- frontend/controllers/addToCartHandler.js
+++ frontend/controllers/addToCartHandler.js
@@ -1,11 +1,14 @@
+let totalPrice = 0; // Initialize total price
+
 export function addToCart(event) {
   const cardButton = event.target; // Get the clicked button
   const cart = document.querySelector('.cart');
-  
+  const totalPriceElement = document.querySelector('.total-price');
+
   const wishCard = cardButton.closest('.wish-card'); // Find the closest wish-card element
-  
+
   const title = wishCard.dataset.title;
-  const price = wishCard.dataset.price;
+  const price = parseFloat(wishCard.dataset.price); // Convert the price to a floating-point number
 
   const div = document.createElement('div');
   div.className = 'cart-item';
@@ -15,4 +18,17 @@ export function addToCart(event) {
   `;
 
   cart.appendChild(div);
+
+  totalPrice += price; // Update total price
+
+  // Remove existing total price display
+  if (totalPriceElement) {
+    totalPriceElement.remove();
+  }
+
+  // Display the total price at the end of the cart
+  const totalPriceDisplay = document.createElement('div');
+  totalPriceDisplay.className = 'total-price';
+  totalPriceDisplay.textContent = `Total: $${totalPrice.toFixed(2)}`;
+  cart.appendChild(totalPriceDisplay);
 }
